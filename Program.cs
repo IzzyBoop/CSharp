77c143f526bbeebcc767ffde8e7afc7a11df1894
SexyJesus <43628079+SexyJesus@users.noreply.github.com>
2018-11-02 09:10:51 -0400

    This is a Joke

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inflloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i < 2; i--)
+            {
+                Console.WriteLine("Fuck You");
+                i++;
+            }
+        }
+    }
+}
